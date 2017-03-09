291123367f5788d9feae2709df7d871656a0857c
suexmex <sulmig29@gmail.com>
2017-03-08 18:31:35 -0600

    First Website Upload

---
+++ scarySite/stuff.js
@@ -0,0 +1,10 @@
+/*function pageAudio() {
+    var ribbit = document.getElementById("page");
+    page.play();
+}*/
+
+/*$('#btn').click(function(e){    
+    $('#uno').fadeOut('slow', function(){
+        $('#uno').fadeIn('slow');
+    });
+});*/
